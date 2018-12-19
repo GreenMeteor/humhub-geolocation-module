8b74dbf3b2feb16d0c06366e58fdf63f2d3de8f0
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-12-20 00:39:39 +0900

    Fix: Make it more translatable

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="panel panel-default">
-        <div class="panel-heading"><strong>Geolocation</strong> <?= Yii::t('GeolocationModule.base', 'configuration') ?></div>
+        <div class="panel-heading"><?= Yii::t('GeolocationModule.base', '<strong>Geolocation</strong> configuration') ?></div>
 
         <div class="panel-body">
             <p><?= Yii::t('GeolocationModule.base', 'Welcome to the admin only area.') ?></p>
