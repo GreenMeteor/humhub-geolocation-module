fadb45615c4731a552e11627e908e7f4d6ab121d
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-12-20 17:49:24 +0900

    Update UserSearch.php

--- models/UserSearch.php
+++ models/UserSearch.php
@@ -7,13 +7,12 @@ use humhub\modules\queue\driver\MySQLCommand;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use humhub\modules\geolocation\models\User;
 use yii\db\QueryBuilder;
 
 /**
  * UserSearch represents the model behind the search form of `humhub\modules\geolocation\models\User`.
  */
-class UserSearch extends User
+class UserSearch extends \humhub\modules\geolocation\models\User
 {
     /**
      * {@inheritdoc}
